2bffd14685f4d13c996039de33e95681d1fa7ffc
Heveraldo Augusto <heveraldoa8@gmail.com>
2025-09-05 12:39:53 -0300

    Atualização ProdutoController

---
+++ src/main/java/br/com/heveraldo/gestao_pedidos/controller/ProdutoController.java
@@ -0,0 +1,34 @@
+package br.com.heveraldo.gestao_pedidos.controller;
+
+import br.com.heveraldo.gestao_pedidos.model.Produto;
+import br.com.heveraldo.gestao_pedidos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @PostMapping
+    public Produto criarProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarProdutoPorId(@PathVariable Long id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        return produto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
