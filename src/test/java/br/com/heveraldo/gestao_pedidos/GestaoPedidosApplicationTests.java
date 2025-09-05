4db1df2a7f63979622421878208d0b3bf6f5620c
Heveraldo Augusto <heveraldoa8@gmail.com>
2025-09-05 11:41:18 -0300

    Primeiro commit

---
+++ src/test/java/br/com/heveraldo/gestao_pedidos/GestaoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heveraldo.gestao_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
