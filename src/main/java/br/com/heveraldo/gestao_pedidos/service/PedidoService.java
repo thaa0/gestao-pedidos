20b88154a9375af49fd2e026b858081f9dd8afb0
Heveraldo Augusto <heveraldoa8@gmail.com>
2025-09-10 14:26:06 -0300

    Correção de imports

--- src/main/java/br/com/heveraldo/gestao_pedidos/service/PedidoService.java
+++ src/main/java/br/com/heveraldo/gestao_pedidos/service/PedidoService.java
@@ -15,7 +15,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional; 
 
 @Service
 public class PedidoService {
